c84f420c972c0611a50e14b15b69c2daef848f73
Matt Cameron <mattlbcameron@gmail.com>
2016-08-26 21:22:40 +1000

    Remove old puma config

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,16 +11,6 @@ set :deploy_via, :remote_cache
 
 # puma
 set :puma_conf, "#{shared_path}/config/puma.rb"
-set :puma_threads, [4, 16]
-set :puma_workers, 0
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.access.log"
-set :puma_error_log, "#{release_path}/log/puma.error.log"
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
 
 # rbenv
 set :rbenv_path, '/home/ubuntu/.rbenv/'
