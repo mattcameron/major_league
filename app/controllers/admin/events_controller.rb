f4278c5796b137e3fa32e41ebd65b6f430527c52
Matt Cameron <mattlbcameron@gmail.com>
2016-08-27 18:12:20 +1000

    Authorise user before admin events controller actions

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,4 +1,4 @@
-class Admin::EventsController < ApplicationController
+class Admin::EventsController < AdminController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   # GET /events
