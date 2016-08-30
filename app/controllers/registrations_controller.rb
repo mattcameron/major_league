ceda90767beb7abe635cfa648f11f94ee9c56191
Matt Cameron <mattlbcameron@gmail.com>
2016-08-30 22:17:53 +1000

    General tidy up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,10 @@ class RegistrationsController <Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
-    resource.update_without_password(params)
+    if params[:password].present?
+      resource.update(params)
+    else
+      resource.update_without_password(params)
+    end
   end
 end
