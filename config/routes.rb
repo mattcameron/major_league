bcdf6415bb3d082fcf64da55df17ed6bddc05b1f
Matt Cameron <mattlbcameron@gmail.com>
2017-06-06 21:35:31 +1000

    WIP Add admin side to leagues

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
     resources :events, except: :show
+    resources :leagues
   end
 end
---
+++ spec/controllers/admin/leagues_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Admin::LeaguesController, type: :controller do
+
+end
